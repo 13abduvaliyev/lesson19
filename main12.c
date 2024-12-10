05c1c1614f8529e5aacecdaba67b0d2766a5982c
13abduvaliyev <abduvaliyevnurbek260>
2024-12-10 18:30:47 +0500

    12-misol

--- main12.c
+++ main12.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main(){
+
+    char text[] = "Hello how are you";
+
+    int count = 0, i = 1;
+
+    while(text[i] !='\0'){
+        if(text[i - 1] != ' ' && text[i] == ' '){
+            count++;
+        }
+        i++;
+    }
+
+    printf("%d\n", count + 1);
+
+    return 0;
+}
