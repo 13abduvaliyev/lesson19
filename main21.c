44f0dc89d65b1f5b8f7680300f5e3fb59d73b384
13abduvaliyev <abduvaliyevnurbek260>
2024-12-10 19:36:06 +0500

    21-misol

--- main21.c
+++ main21.c
@@ -1,28 +1,6 @@
 #include <stdio.h>
-#include <string.h>  
-
-int scan(char m[], char n[]){
-    if(m != n){
-       return 1;  
-    }else{
-       return 0; 
-    }
-}
 
 int main(){
-    char m[100], n[100];  
-
-    printf("1-matnni kiriting: ");
-    fgets(m, sizeof(m), stdin);  
-
-    printf("2-matnni kiriting: ");
-    fgets(n, sizeof(n), stdin); 
-
-    if(scan(m, n)){
-       printf("teng\n");
-    }else{
-       printf("teng emas\n");
-    }
 
     return 0;
 }
